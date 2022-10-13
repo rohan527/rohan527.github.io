c51c930ec71a1e1d85a2519a8b8d712fee5a190c
Rohan Ghosalkar <rohan.ghosalkar00@gmail.com>
2022-10-13 15:19:29 -0700

    Adding edited files

---
+++ js/typed.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+var options = {
+  strings: ["I'm a Master's student at UCSC", 'I work on ML and AI projects'],
+  typeSpeed: 120,
+  backSpeed: 50,
+  backDelay: 1500
+  // loop: true
+};
+
+var typed = new Typed('#headline', options);
